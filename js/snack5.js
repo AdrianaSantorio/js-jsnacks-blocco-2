58bfc4bd61dbb3306ee30dee1a4c74113addfb21
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-13 21:55:39 +0100

    completed snack 5

---
+++ js/snack5.js
@@ -0,0 +1,53 @@
+
+/*
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro.
+*/
+
+//1 - decidi la lunghezza dell' array
+//2 - inserisci numeri casuali finchè l' array non è di quella lunghezza
+//3 - individua l' array con meno elementi
+//4 - Aggiungi elementi casuali all’array che ha meno elementi,fino a quando ne avrà tanti quanti l’altro.
+
+console.log('JS OK');
+
+const firstList = [];
+const secondList = [];
+
+const firstListLength = Math.floor(Math.random()*100 +1);
+console.log(firstListLength);
+const secondListLength = Math.floor(Math.random()*100 +1);
+console.log(secondListLength);
+
+//1
+do {
+    const number = Math.floor(Math.random()*100 +1);
+    firstList.push(number);
+
+} while (firstList.length < firstListLength)
+
+console.log(firstList);
+
+//1
+do {
+    const number = Math.floor(Math.random()*100 +1);
+    secondList.push(number);
+
+} while (secondList.length < secondListLength)
+
+console.log(secondList);
+
+
+while (!(secondList.length === firstList.length)) {
+
+    if  (secondList.length > firstList.length) {
+        const number = Math.floor(Math.random()*100 +1);
+        firstList.push(number);
+    } else if (secondList.length < firstList.length){
+        const number = Math.floor(Math.random()*100 +1);
+        secondList.push(number);
+    }
+}
+
+console.log(firstList.length,secondList.length);
